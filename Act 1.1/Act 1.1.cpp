d5c7b7213e657f5e5cc227a4e61c8e1feb3a0234
Alan Soto <alan.sobanda@gmail.com>
2026-09-01 11:45:30 -0600

    hice mas simple ver los casos

--- Act 1.1/Act 1.1.cpp
+++ Act 1.1/Act 1.1.cpp
@@ -3,29 +3,25 @@
 
     int main() {
         // Caso 1: Esperado: 1
-        std::cout << "Caso 1 (n = 1)" << std::endl;
-        std::cout << "Esperado: 1" << std::endl;
+        std::cout << "Caso 1 (Se espera 1)" << std::endl;
         std::cout << "Iterativa:" << Calculos::sumaIterativa(1) << std::endl;
         std::cout << "Recursiva:" << Calculos::sumaRecursiva(1) << std::endl;
         std::cout << "Directa:" << Calculos::sumaDirecta(1) << "\n";
 
         // Caso 2: Esperado: 15
-        std::cout << "Caso 2 (n = 5)" << std::endl;
-        std::cout << "Esperado:15" << std::endl;
+        std::cout << "Caso 2 (Se espera 15)" << std::endl;
         std::cout << "Iterativa:" << Calculos::sumaIterativa(5) << std::endl;
         std::cout << "Recursiva:" << Calculos::sumaRecursiva(5) << std::endl;
         std::cout << "Directa:" << Calculos::sumaDirecta(5) << "\n";
 
         // Caso 3: Esperado: 210
-        std::cout << "Caso 3 (n = 20)" << std::endl;
-        std::cout << "Esperado:210" << std::endl;
+        std::cout << "Caso 3 (Se espera 210)" << std::endl;
         std::cout << "Iterativa:" << Calculos::sumaIterativa(20) << std::endl;
         std::cout << "Recursiva:" << Calculos::sumaRecursiva(20) << std::endl;
         std::cout << "Directa:" << Calculos::sumaDirecta(20) << "\n";
 
         // Caso 4:  Esperado: 5050
-        std::cout << "Caso 4 (n = 100)" << std::endl;
-        std::cout << "Esperado:5050" << std::endl;
+        std::cout << "Caso 4 (Se espera 5050)" << std::endl;
         std::cout << "Iterativa:" << Calculos::sumaIterativa(100) << std::endl;
         std::cout << "Recursiva:" << Calculos::sumaRecursiva(100) << std::endl;
         std::cout << "Directa:" << Calculos::sumaDirecta(100) << "\n";
